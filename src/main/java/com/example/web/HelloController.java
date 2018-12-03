426d53908539b94dfecd9e9b82727f4f3627afd3
Tianqi Ren <tqren@thoughtworks.com>
2018-12-03 15:37:55 +0800

    [colin] return index page by thymeleaf

--- src/main/java/com/example/web/HelloController.java
+++ src/main/java/com/example/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,8 @@ public class HelloController {
     }
 
     @RequestMapping("/")
-    public String index() {
-        return "/index.html";
+    public String index(ModelMap map) {
+        map.addAttribute("host", "http://colining.cn");
+        return "index";
     }
 }
